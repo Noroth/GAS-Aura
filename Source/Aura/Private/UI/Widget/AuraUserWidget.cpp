6116a357beb5c5aed976ccd00ffacd32f1fd66cc
Noroth <ludwig.bedacht@gmail.com>
2024-02-17 02:09:22 +0100

    add view and controller classes

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Noroth
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
